a872d632d45417c34a58f3b79d38afd3db03c847
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-03-04 20:45:32 +1100

    fixed cors bug

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 // CORS - multiple domains can be separated by commas
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Accept,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
