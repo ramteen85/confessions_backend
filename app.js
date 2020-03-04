3bb95e5c9c88de770288434662e415041e938bda
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-03-04 19:41:54 +1100

    fixed cors bug

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
 
@@ -33,11 +32,11 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 // CORS - multiple domains can be separated by commas
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Accept,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
