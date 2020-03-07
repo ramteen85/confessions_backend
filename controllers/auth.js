22e87d7f53274e347e6f6317335828590cfa3980
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-03-07 20:39:42 +1100

    added async functionality to auth controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ exports.register = async(req, res, next) => {
     };
 };
 
-exports.saveTrueUserLoc = (req, res, next) => {
+exports.saveTrueUserLoc = async(req, res, next) => {
 
     // get token
     // get latitude
@@ -69,10 +69,11 @@ exports.saveTrueUserLoc = (req, res, next) => {
         throw err;
     }
 
-
-    // get user records
-    User.findOne({_id: decodedToken.userId})
-    .then(usr => {
+    try 
+    {
+        // get user records
+        let usr = await User.findOne({_id: decodedToken.userId})
+        
         // got user records
         // console.log(user);
 
@@ -83,42 +84,35 @@ exports.saveTrueUserLoc = (req, res, next) => {
 
         if(usr.trueLoc === false) {
             // have latitude and longitude - get other info
-            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${api}`)
-            .then(result => {
-                // got location!
+            let result = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${api}`);
+            
+            // got location!
 
-                    // save callback results
-                    usr.streetNumber = result.data.results[0].address_components[0].long_name;
-                    usr.streetName = result.data.results[0].address_components[1].long_name;
-                    usr.suburb = result.data.results[0].address_components[2].long_name;
-                    usr.state = result.data.results[0].address_components[4].long_name;
-                    usr.country = result.data.results[0].address_components[5].long_name;
-                    usr.countryCode = result.data.results[0].address_components[5].short_name;
-                    usr.postcode = result.data.results[0].address_components[6].long_name;
-                    usr.trueLoc = true;
-                    usr.save();
+            // save callback results
+            usr.streetNumber = result.data.results[0].address_components[0].long_name;
+            usr.streetName = result.data.results[0].address_components[1].long_name;
+            usr.suburb = result.data.results[0].address_components[2].long_name;
+            usr.state = result.data.results[0].address_components[4].long_name;
+            usr.country = result.data.results[0].address_components[5].long_name;
+            usr.countryCode = result.data.results[0].address_components[5].short_name;
+            usr.postcode = result.data.results[0].address_components[6].long_name;
+            usr.trueLoc = true;
+            await usr.save();
 
-                    // store saved user data in a return object for the client
-                    clientReturn = {
-                        suburb: usr.suburb,
-                        state: usr.state,
-                        country: usr.country,
-                        countryCode: usr.countryCode,
-                        postcode: usr.postcode,
-                        trueLoc: usr.trueLoc
-                    };
+            // store saved user data in a return object for the client
+            clientReturn = {
+                suburb: usr.suburb,
+                state: usr.state,
+                country: usr.country,
+                countryCode: usr.countryCode,
+                postcode: usr.postcode,
+                trueLoc: usr.trueLoc
+            };
 
-                    //return json
-                    res.status(200).json({
-                        message: "User true location saved!",
-                        user: clientReturn
-                    });
-            })
-            .catch((err) => {
-                console.log(err);
-                err.statusCode = 500;
-                err.message = "Google maps failed!";
-                throw err;
+            //return json
+            res.status(200).json({
+                message: "User true location saved!",
+                user: clientReturn
             });
         } else {
             // true location data exists
@@ -127,16 +121,16 @@ exports.saveTrueUserLoc = (req, res, next) => {
                 message: "data exists"
             });
         }
-    })
-    .catch(err => {
+    }
+    catch(err) {
         // couldnt get user records therefore invalid token
         console.log(err);
         err.statusCode = 500;
         throw err;
-    });
+    };
 };
 
-exports.saveRoughUserLoc = (req, res, next) => {
+exports.saveRoughUserLoc = async(req, res, next) => {
     token = req.body.data.token;
     ip = req.body.data.ip;
 
@@ -149,87 +143,81 @@ exports.saveRoughUserLoc = (req, res, next) => {
         throw err;
     }
 
-    // now need to get geolocation from ip
-    axios.get(`${process.env.GEOSITE}?ip=` + ip)
-    .then(result => {
+    try {
+        // now need to get geolocation from ip
+        let result = axios.get(`${process.env.GEOSITE}?ip=` + ip);
+
         // success managed to get location without permission (not accurate though - save later!)!
         console.log('got location!');
         console.log(result);
 
         // get user records
-        User.findOne({_id: decodedToken.userId})
-        .then(usr => {
-            // got user records
+        let usr = await User.findOne({_id: decodedToken.userId});
 
-            // should check to see if a true location exists. if so, push existing location to location array
-            if(usr.trueLoc === true) {
-                // true location exists
+        // got user records
 
-                // arrayTemp = usr.prevLocs || [];
-                // arrayTemp.push({
-                //     trueLoc: usr.trueLoc,
-                //     country: usr.country,
-                //     countryCode: usr.countryCode,
-                //     state: usr.state,
-                //     suburb: usr.suburb,
-                //     streetNumber: usr.streetNumber,
-                //     streetName: usr.streetName,
-                //     postcode: usr.postcode
-                // });
-            }
-            else {
+        // should check to see if a true location exists. if so, push existing location to location array
+        if(usr.trueLoc === true) {
+            // true location exists (maybe update tracking later??)
 
-                // update user values and reset true location back to false
-                usr.trueLoc = false;
-                usr.country = result.data.geoplugin_countryName;
-                usr.countryCode = result.data.geoplugin_countryCode;
-                usr.state = result.data.geoplugin_regionName;
-                usr.suburb = result.data.geoplugin_city;
-                lng = +result.data.geoplugin_longitude;
-                lat = +result.data.geoplugin_latitude;
+            // arrayTemp = usr.prevLocs || [];
+            // arrayTemp.push({
+            //     trueLoc: usr.trueLoc,
+            //     country: usr.country,
+            //     countryCode: usr.countryCode,
+            //     state: usr.state,
+            //     suburb: usr.suburb,
+            //     streetNumber: usr.streetNumber,
+            //     streetName: usr.streetName,
+            //     postcode: usr.postcode
+            // });
+        }
+        else {
+
+            // update user values and reset true location back to false
+            usr.trueLoc = false;
+            usr.country = result.data.geoplugin_countryName;
+            usr.countryCode = result.data.geoplugin_countryCode;
+            usr.state = result.data.geoplugin_regionName;
+            usr.suburb = result.data.geoplugin_city;
+            lng = +result.data.geoplugin_longitude;
+            lat = +result.data.geoplugin_latitude;
 
 
-                // geotagging
-                usr.location = {
-                    type: "Point",
-                    coordinates: [lng,lat]
-                };
-
-                //save user
-                usr.save();
-            }
-
-            usrReturn = {
-                country: usr.country,
-                countryCode: usr.countryCode,
-                state: usr.state,
-                suburb: usr.suburb,
-                location: usr.location.coordinates,
-                trueLoc: usr.trueLoc,
+            // geotagging
+            usr.location = {
+                type: "Point",
+                coordinates: [lng,lat]
             };
 
-            console.log('responding...');
+            //save user
+            await usr.save();
+        }
 
-            // respond with client info
-            res.status(200).json({user: usrReturn});
-        })
-        .catch(err => {
-            // couldnt get user records therefore invalid token
-            console.log(err);
-            err.statusCode = 500;
-            throw err;
-        });
-    })
-    .catch(err => {
+        usrReturn = {
+            country: usr.country,
+            countryCode: usr.countryCode,
+            state: usr.state,
+            suburb: usr.suburb,
+            location: usr.location.coordinates,
+            trueLoc: usr.trueLoc,
+        };
+
+        console.log('responding...');
+
+        // respond with client info
+        res.status(200).json({user: usrReturn});
+    }
+    catch(err) {
         // let user know page cannot load until they allow geolocation
         err.message = 'could not get location goddamnit';
         err.statusCode = 500;
         next(err);
-    });
+    };
 }
 
 
-exports.getUserLoc = (req, res, next) => {
+exports.getUserLoc = async(req, res, next) => {
 
     // get user location details from database
 
@@ -245,9 +233,9 @@ exports.getUserLoc = (req, res, next) => {
         next(err)
     }
 
-    // get user records
-    User.findOne({_id: decodedToken.userId})
-    .then(result => {
+    try {
+        // get user records
+        let result = await User.findOne({_id: decodedToken.userId});
 
         // store user location details to send back
         const locInfo = {
@@ -263,49 +251,43 @@ exports.getUserLoc = (req, res, next) => {
 
         // send back user location details from database
         res.status(200).json({ locInfo });
-    })
-    .catch(err => {
+    }
+    catch(err) {
         err.statusCode = 500;
         err.message = "Error finding user in database. Invalid token?";
         throw err;
-    });
+    };
 };
 
-exports.getUserById = (req, res, next) => {
-     // get tokens
-     token = req.body.data.token;
-     id = req.body.data.id;
+exports.getUserById = async(req, res, next) => {
+    // get tokens
+    token = req.body.data.token;
+    id = req.body.data.id;
 
-     
+    // verify token
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.SECRET_KEY)
+    } catch(err) {
+        res.status(200).json({
+            message: "An error occurred"
+        });
+    }
+    try {
+        let result = await User.findById(id).populate('confessionsList').populate('chatList');
 
-     // verify token
-     let decodedToken;
-     try {
-         decodedToken = jwt.verify(token, process.env.SECRET_KEY)
-     } catch(err) {
-         res.status(200).json({
-             message: "An error occurred"
-         });
-     }
-
-     const user = User.findById(id)
-            .populate('confessionsList')
-            .populate('chatList')
-            .then(result => {
-                console.log("id: " + id);
-                console.log(result);
-                res.json({
-                    user: {
-                        _id: result._id,
-                        nickname: result.nickname,
-                        chatList: result.chatList
-                    }
-                });
-            })
-            .catch(err => {
-                err.statusCode = 500;
-                throw err;
-            });
+        res.json({
+            user: {
+                _id: result._id,
+                nickname: result.nickname,
+                chatList: result.chatList
+            }
+        });
+    }
+    catch(err) {
+        err.statusCode = 500;
+        next(err);
+    };
 
 }
 
@@ -365,24 +347,24 @@ exports.getChatList = async(req, res, next) => {
 
 }
 
-exports.login = (req, res, next) => {
+exports.login = async(req, res, next) => {
 
     // console.log(req.body.result.email);
 
     const email = req.body.result.email;
     const password = req.body.result.password;
     let loadedUser;
-    User.findOne({email: email})
-    .then(user => {
+
+    try {
+        let user = User.findOne({email: email});
+
         if(!user) {
             const error = new Error('Invalid Username/Password');
             error.statusCode = 401;
             throw error;
         }
         loadedUser = user;
-        return bcrypt.compare(password, user.password);
-    })
-    .then(isEqual => {
+        let isEqual = await bcrypt.compare(password, user.password);
         if(!isEqual) {
             const error = new Error('Invalid Username/Password');
             error.statusCode = 401;
@@ -396,16 +378,16 @@ exports.login = (req, res, next) => {
             { expiresIn: '12h' }
         );
         res.status(200).json({ token: token, userId: loadedUser._id.toString() });
-    })
-    .catch(err => {
+    }
+    catch(err) {
         if(!err.statusCode) {
             err.statusCode = 500;
         }
         next(err);
-    });
+    };
 };
 
-exports.getAllUsers = (req, res, next) => {
+exports.getAllUsers = async(req, res, next) => {
 
     token = req.body.data.token;
 
@@ -418,18 +400,18 @@ exports.getAllUsers = (req, res, next) => {
         next(err)
     }
 
-    User.find({})
-    .populate('confessionsList')
-    .populate('chatList')
-    .then(users => {
-        console.log(users);
+    try {
+
+        let users = await User.find({}).populate('confessionsList').populate('chatList');
+
         res.status(200).json({
             message: 'All users',
             users: users
         });
-    })
-    .catch(err => {
+
+    }
+    catch(err) {
         err.statusCode = 500;
         next(err);
-    })
+    }
 }
