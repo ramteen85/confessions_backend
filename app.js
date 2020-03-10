ce5ea37b8e32ab46476a3f7e12e25b42041f1ae6
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-03-10 23:40:53 +1100

    added image upload functionality to cloudinary

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const fileStorage = multer.diskStorage({
         cb(null, path.resolve(__dirname, 'build'));
     },
     filename: (req, file, cb) => {
-        fs.readdir( 'uploads', function(error, files) {  
+        fs.readdir( 'build', function(error, files) {  
             var totalFiles = files.length; // return the number of files
             cb(null, totalFiles + '-' + file.originalname);
         });
